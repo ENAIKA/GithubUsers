cf6e18c8f431b6a78e34237aa735bfb227093492
ENAIKA <naikaesther5@gmail.com>
2020-04-12 20:17:36 +0300

    add HttpClientModule to imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponentComponent } from './Components/user-component/user-component.component';
+import { RepoComponentComponent } from './Components/repo-component/repo-component.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponentComponent,
+    RepoComponentComponent,
+    NotFoundComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
