e5868220279a73fe8e2c296b6701e01a2b6afe29
ENAIKA <naikaesther5@gmail.com>
2020-04-13 13:10:48 +0300

    add route paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import {NotFoundComponent} from '../app/Components/not-found/not-found.component';
+import {RepoComponentComponent} from '../app/Components/repo-component/repo-component.component';
+import {UserComponentComponent} from '../app/Components/user-component/user-component.component';
+const routes: Routes = [
+  
+  { path:'github_users', component:UserComponentComponent},
+  { path:'user_repos', component:RepoComponentComponent},
+  { path: '', redirectTo:'/github_users', pathMatch:'full'},
+  { path:'**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
